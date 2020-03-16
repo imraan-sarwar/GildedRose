7260eb71482d84d109896cdd36f23e0e79dfb03f
Imraan <imraan.sarwar@inthestyle.com>
2020-03-16 15:25:50 +0000

    add model which describes a sellable item

---
+++ src/GildedRose/Domain/Models/SellingItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models
+{
+    public class SellingItem
+    {
+        public string Name { get; set; }
+
+        public int Quality { get; set; }
+
+        public int SellingDays { get; set; }
+    }
+}
