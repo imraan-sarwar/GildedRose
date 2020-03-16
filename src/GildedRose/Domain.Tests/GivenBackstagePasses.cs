e07caacad17a939397d183cf5d6bc03949cc51be
Imraan <imraan.sarwar@inthestyle.com>
2020-03-16 15:56:16 +0000

    add class for backstage passes

---
+++ src/GildedRose/Domain.Tests/GivenBackstagePasses.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Inventory;
+using Domain.Inventory.Items;
+using Domain.Models;
+using NUnit.Framework;
+using System;
+
+namespace Domain.Tests
+{
+    public class GivenBackstagePasses
+    {
+        private SellingItem _sellingItem;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sellingItem = new SellingItem()
+            {
+                Name = "Backstage passes",
+                Quality = 1,
+                SellingDays = 1
+            };
+        }
+
+        [Test]
+        public void SellingDaysDecreasesByOne()
+        {
+            var b = new BackstagePasses(_sellingItem);
+            Assert.AreEqual(0, b.SellingDays);
+        }
+     
+    }
+}
---
+++ src/GildedRose/Domain/Inventory/BackstagePasses.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+
+namespace Domain.Inventory.Items
+{
+    public class BackstagePasses : InventoryItem
+    {
+
+        public BackstagePasses(SellingItem sellingItem) : base(sellingItem)
+        {
+            Process();
+        }
+
+        public void Process()
+        {
+            DecreaseSellingDay();
+
+            Validate();
+
+        }
+    }
+}
