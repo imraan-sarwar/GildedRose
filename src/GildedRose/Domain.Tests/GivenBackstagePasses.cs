eb172c0d1bec91297cc08fdcaca5b1357a2b81e6
Imraan <imraan.sarwar@inthestyle.com>
2020-03-16 18:27:28 +0000

    add the rest of the rules for back stage passes. Add more tests!

--- src/GildedRose/Domain.Tests/GivenBackstagePasses.cs
+++ src/GildedRose/Domain.Tests/GivenBackstagePasses.cs
@@ -28,6 +28,7 @@ namespace Domain.Tests
             Assert.AreEqual(0, b.SellingDays);
         }
 
+
         [Test]
         public void QualityIsNeverAbove50()
         {
@@ -54,5 +55,47 @@ namespace Domain.Tests
             Assert.AreEqual(16, b.Quality);
         }
 
+        public void SellingDaysIsLessThan10_Then_QualityIncreasesByTwo()
+        {
+            _sellingItem.Quality = 20;
+            _sellingItem.SellingDays = 9;
+            var b = new BackstagePasses(_sellingItem);
+            Assert.AreEqual(22, b.Quality);
+        }
+
+        public void SellingDaysIs10_Then_QualityIncreasesByTwo()
+        {
+            _sellingItem.Quality = 20;
+            _sellingItem.SellingDays = 10;
+            var b = new BackstagePasses(_sellingItem);
+            Assert.AreEqual(22, b.Quality);
+        }
+
+        [Test]
+        public void SellingDaysIsLessThan5_Then_QualityIncreasesByThree()
+        {
+            _sellingItem.Quality = 20;
+            _sellingItem.SellingDays = 4;
+            var b = new BackstagePasses(_sellingItem);
+            Assert.AreEqual(23, b.Quality);
+        }
+
+        [Test]
+        public void SellingDaysIs5_Then_QualityIncreasesByThree()
+        {
+            _sellingItem.Quality = 20;
+            _sellingItem.SellingDays = 5;
+            var b = new BackstagePasses(_sellingItem);
+            Assert.AreEqual(23, b.Quality);
+        }
+
+        [Test]
+        public void SellingDaysIs1_Then_QualityIs0()
+        {
+            _sellingItem.Quality = 0;
+            _sellingItem.SellingDays = 1;
+            var b = new BackstagePasses(_sellingItem);
+            Assert.AreEqual(0, b.Quality);
+        }
     }
 }
--- src/GildedRose/Domain/Inventory/BackstagePasses.cs
+++ src/GildedRose/Domain/Inventory/BackstagePasses.cs
@@ -18,6 +18,18 @@ namespace Domain.Inventory.Items
             {
                 Quality = Quality + 1;
             }
+            else if (SellingDays <= 10 && SellingDays > 5)
+            {
+                Quality = Quality + 2;
+            }
+            else if (SellingDays <= 5 && SellingDays >= 1)
+            {
+                Quality = Quality + 3;
+            }
+            else
+            {
+                Quality = 0;
+            }
 
             Validate();
 
